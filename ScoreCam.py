53a62dd4c1e8f3bceeb3e6d2c04263c5e04b4232
NickWils98 <nick.wils@student.uantwerpen.be>
2023-08-23 02:56:55 +0200

    run models

--- ScoreCam.py
+++ ScoreCam.py
@@ -20,7 +20,7 @@ import numpy as np
 from PIL import Image
 import os
 
-def score_cam_batch(model, model_path, val_loader, orignal_data, device, image_list, plot_path):
+def score_cam_batch(model, model_path, val_loader, device, image_list, plot_path, batch_nr):
     image_list = sorted(image_list)
     plot_path = os.path.join(plot_path, "ScoreCAM")
     # Load pre-trained EfficientNet-B0 model
@@ -30,26 +30,19 @@ def score_cam_batch(model, model_path, val_loader, orignal_data, device, image_l
     model.eval()
     model.to(device)
 
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
-    val_dataset = ImageFolder(os.path.join(orignal_data, "validation"), transform=transform)
-
     for batch_rotated_images, labels in val_loader:
         all_images = []
         rotated_labels = []
 
         for j, rotated_image_list in enumerate(batch_rotated_images):
-            all_images.extend(rotated_image_list)
-            rotated_labels.extend([j] * len(rotated_image_list))
+            if j == batch_nr:
+                all_images.extend(rotated_image_list)
+                rotated_labels.extend([j] * len(rotated_image_list))
         for img in all_images:
             if counter in image_list:
                 image_list.remove(counter)
                 scorecam = ScoreCAM(model, target_layer=model.features[8])
                 # Choose an image from the validation dataset for visualization
-                og_image = val_dataset[counter][0]
                 image = img.unsqueeze(0).to(device)
 
                 # Get the class index with the highest probability
@@ -65,7 +58,7 @@ def score_cam_batch(model, model_path, val_loader, orignal_data, device, image_l
                                    predicted_class)  # Use .item() to get the class index as a scalar
                 # Overlayed on the image
                 for name, cam in zip(scorecam.target_names, heatmap):
-                    result = overlay_mask(to_pil_image(og_image), to_pil_image(cam.squeeze(0), mode='F'), alpha=0.5)
+                    result = overlay_mask(to_pil_image(img), to_pil_image(cam.squeeze(0), mode='F'), alpha=0.5)
                     plt.imshow(result)
                     plt.axis('off')
                     plt.title(f'{counter}')
